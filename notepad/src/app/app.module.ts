68c29a3e8c905749aefe919246cf120199eb1220
Srija Nair <srijanair92@gmail.com>
2022-10-19 16:13:29 +0300

    Added Table
    Simple table with dummy action buttons

--- notepad/src/app/app.module.ts
+++ notepad/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserlistComponent } from './src/app/userlist/userlist.component';
+import { UserlistComponent } from './components/userlist/userlist.component';
 import { AddeditusersComponent } from './components/addeditusers/addeditusers.component';
 
 @NgModule({
