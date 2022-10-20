59e0802ccf50acdb9a56745ebf02ca09a219ad70
Srija Nair <srijanair92@gmail.com>
2022-10-20 12:37:51 +0300

    Dynamic Table Data

--- notepad/src/app/components/userlist/userlist.component.ts
+++ notepad/src/app/components/userlist/userlist.component.ts
@@ -1,4 +1,13 @@
+import { AstTransformer } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { doesNotMatch } from 'assert';
+
+export interface UserList {
+  firstname : string;
+  lastname: string;
+  contact: number;
+  email: string;
+}
 
 @Component({
   selector: 'app-userlist',
@@ -7,9 +16,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserlistComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit(): void {
   }
 
+  public User: UserList[] = [
+    { firstname: 'Arsan', lastname: 'Miths', contact: 9998899877, email:'arsandoe@gmail.com' },
+    { firstname: 'Bobby', lastname: 'Morris', contact: 1019998080, email:'bobbymorris@gmail.com' },
+    { firstname: 'Charlie', lastname: 'Davis', contact: 1110007676, email:'charliedavis@gmail.com' },
+    { firstname: 'Danny', lastname: 'Wilson', contact: 808080999, email:'dannywilson@gmail.com' },
+    { firstname: 'Ena', lastname: 'Wright', contact: 808080999, email:'dannywilson@gmail.com' },
+  ]
 }
